ffc28b5f0e7591e9847d86737efc9af0991a0fc8
Ofagbor Michael <eihmenormike@gmail.com>
2024-07-19 15:02:28 -0700

    import component change

--- components/Services.js
+++ components/Services.js
@@ -8,7 +8,7 @@ const Services = () => {
   <section className='bg-services bg-cover bg-no-repeat bg-center min-h-[862px] py-8'>
     <div className='container mx-auto h-[862px]'>
       <h2 className='h2 mb-12'>Our Services</h2>
-      <serviceSlider />
+      <ServiceSlider />
     </div>
   </section>
   );
